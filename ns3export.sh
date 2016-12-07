90db0cc01b75ffdb901ce718ab6d610a46c61495
TobiasAnderssonq <anderssontobias93@gmail.com>
2016-12-07 13:00:38 +0100

    initial commit

---
+++ ns3export.sh
@@ -0,0 +1,4 @@
+export PYTHONPATH=$PYTHONPATH:/home/user/ns-allinone-3.26/ns-3.26/build/bindings/python:/home/user/ns-allinone-3.26/ns-3.26/src/visualizer:/home/user/ns-allinone-3.26/pybindgen-0.17.0.post57+nga6376f2
+export PATH=/home/user/ns-allinone-3.26/ns-3.26/build/src/fd-net-device:/home/user/ns-allinone-3.26/ns-3.26/build/src/tap-bridge:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user/ns-allinone-3.26/ns-3.26/build
+export NS3_EXECUTABLE_PATH=/home/user/ns-allinone-3.26/ns-3.26/build/src/fd-net-device:/home/user/ns-allinone-3.26/ns-3.26/build/src/tap-bridge
